c5aaf9513473f53c2cb2c3deac493d5c044b25b2
MuhamadAdrian <madrian.septiana@gmail.com>
2020-06-08 11:32:55 +0700

    fitur

--- vendor/ui.js
+++ vendor/ui.js
@@ -4,7 +4,9 @@ ScrollOut({
 
 let toggleButton = document.querySelector('.fba');
 let mobileMenu = document.querySelector('.menu');
+let medsos = document.querySelector('.medsos');
 toggleButton.addEventListener('click', function(e){
     mobileMenu.classList.toggle('show');
+    medsos.classList.toggle('show');
     toggleButton.classList.toggle('clicked');
 })
